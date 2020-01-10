96e113cbb1f5fc7d93bb76b08117e1b6e47d21e4
Luisa Pimentel <luisa.pimentel@minutrade.com>
2020-01-10 18:02:47 -0300

    Variáveis e Operadores - AULA 02 / ALURA

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,14 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(altura); 
